68758e986aee2bdd7996cfb04063ef8f61b6a8db
Viktor Krückl <viktor.krueckl@ams-osram.com>
2022-09-19 10:18:18 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='eureka_requests',
+    url="https://dev.azure.com/OsramDS/Tools/_git/eureka-requests",
+    version=os.getenv('PYLIB_VERSION', '0.0.0'),
+    packages=find_packages(),
+    license="MIT",
+    keywords="Use eureka to make a request at different locations",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'requests',
+        'py-eureka-client'
+    ]
+)
